2b99ff1f0ad8d093769f65565afd1e9a00c900d0
CamiloGiraldo-dev <camilin.giraldo20@gmail.com>
2026-04-24 10:03:29 -0500

    En el modelo agregue los atrivutos, constructor , get, set y el metodo

--- src/main/java/modelo/Cliente.java
+++ src/main/java/modelo/Cliente.java
@@ -9,5 +9,48 @@ package modelo;
  * @author dinah
  */
 public class Cliente {
+
     
+     private String nombre;
+     private double peso;
+     private int altura;
+    
+    
+    public Cliente(String nombre, double peso, int altura) {
+        this.nombre = nombre;
+        this.peso = peso;
+        this.altura = altura;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+     
+     public double  imc(double peso, int altura){
+         
+         double resultado = peso / ( (altura/100) * (altura/100) );
+         
+         return resultado; 
+     }
+     
 }
